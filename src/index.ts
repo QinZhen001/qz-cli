3bb21d5e2eb31d437cdd72d2dcf79753c200df9e
qinzhen <qz519189636@qq.com>
2024-05-20 18:01:46 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Command } from "commander"
+import { run } from "./run"
 
 const program = new Command()
 
 program
+  .name("buzz-cli")
   .version("1.0.0")
-  .description("Buzz CLI")
-  .option("-n, --name <type>", "Add your name")
-  .action((options) => {
-    console.log(`Hey, ${options.name}!`)
+  .description("Buzz CLI is a command line tool to buzz your strings!")
+
+program
+  .command("run")
+  .description("Run description xxxxxxxxxxxxxxxxxxx")
+  .argument("<string>", "string to split")
+  .option("--first", "display just the first substring")
+  .option("-s, --separator <char>", "separator character", ",")
+  .action((str, options) => {
+    run(str, options)
   })
+
+// program.command('create')
+// ...
 
 program.parse(process.argv)
